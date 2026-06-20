83e1c9020547aaae74dc61f4aa1cc11cbe9bf2a1
hexzii <hexzii@nichijou.moe>
2026-06-21 02:53:04 +0800

    separate configs

---
+++ dotfiles/dot_config/hypr/plugins/hypr-dynamic-cursors.lua
@@ -0,0 +1,16 @@
+-- hypr-dynamic-cursors - shake to find cursor
+if hl.plugin.dynamic_cursors then
+  hl.config({ plugin = { dynamic_cursors = {
+    enabled = true,
+    mode = "none",   -- no tilt/rotate/stretch, just shake to find
+    shake = {
+      enabled = true,
+      threshold = 6.0,
+      base = 6.0,
+      influence = 0.0,
+      speed = 0.0,
+      timeout = 400,
+    },
+    hyprcursor = { enabled = true, nearest = 1 },
+  }}})
+end
